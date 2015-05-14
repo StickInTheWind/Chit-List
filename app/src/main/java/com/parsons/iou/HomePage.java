1e8ec5ea9045ee57e0751c23978ecea15b993314
StickInTheWind <larry.parsons@aol.com>
2015-05-14 15:43:36 -0700

    Submit with emulator able to run.
    Emulator is able to display app

--- app/src/main/java/com/parsons/iou/HomePage.java
+++ app/src/main/java/com/parsons/iou/HomePage.java
@@ -4,22 +4,31 @@ package com.parsons.iou;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.R.layout;
+import android.os.Bundle;
 import android.view.View;
 
 
 
 public class HomePage extends Activity{
-    protected void onCreate() {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    };
+//
+//    protected void onCreate() {
+//        setContentView(R.layout.activity_home_page);
+//
+//
+//    }
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
-
-    }
     public void openFriendsTab(View view){
+      // This adds the "listener" to make the button go from one activity to another. The button in the xml must have an onClick methed.
         Intent intent = new Intent(this, FriendsTab.class);
+        startActivity(intent);
     }
+
     public void openWidgets(View view){
         //Intent intent = new Intent(this, FriendsTab.class);
     }
