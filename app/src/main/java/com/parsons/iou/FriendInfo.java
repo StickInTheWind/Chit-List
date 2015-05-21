04aebd6f84feb86b920f0477b177b5c45ec44a1a
Ed Ramirez <blueeaidle360@yahoo.com>
2015-05-21 15:21:34 -0700

    New FriendInfo class

---
+++ app/src/main/java/com/parsons/iou/FriendInfo.java
@@ -0,0 +1,18 @@
+package com.parsons.iou;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Edward on 5/21/2015.
+ */
+public class FriendInfo extends Activity{
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_friend_info);
+
+        };
+    }
+
