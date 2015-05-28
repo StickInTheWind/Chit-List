73ec0b2e7045c66a31bc5f26c85f8b7c39120e13
Ed Ramirez <blueeaidle360@yahoo.com>
2015-05-28 15:42:03 -0700

    New layout on friends and friends info

--- app/src/main/java/com/parsons/iou/FriendsTab.java
+++ app/src/main/java/com/parsons/iou/FriendsTab.java
@@ -2,8 +2,11 @@ package com.parsons.iou;
 
 import com.parsons.iou.util.SystemUiHider;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 /**
@@ -18,6 +21,19 @@ public class FriendsTab extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_tab);
+        ListView friendsList = (ListView)findViewById(R.id.listView);
+        friendsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                openFriendInfo(view);
+            }
+        });
 
     };
+
+    public void openFriendInfo(View view){
+        // This adds the "listener" to make the button go from one activity to another. The button in the xml must have an onClick methed.
+        Intent intent = new Intent(this, FriendInfo.class);
+        startActivity(intent);
+    }
 }
